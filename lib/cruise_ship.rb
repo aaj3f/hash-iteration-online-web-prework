61563b03df9b0af2b7a758e98a350004b678e449
Andrew Johnson <ajohnson.uva@gmail.com>
2018-10-19 13:28:34 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,6 +6,12 @@
 # suite_e: "Crumpet the Elf"
 # }
 
+winner = ""
 def select_winner(passengers)
-  # add the code snippet here!
+  passengers.each do |suite, name|
+    if suite = "suite_a" && name.starts_with?("A")
+      winner = name
+    end
+  end
+  winner
 end
