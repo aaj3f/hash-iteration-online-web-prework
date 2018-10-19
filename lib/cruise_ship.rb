57ccd9e686ae3f82b7a66ac3da64137ba662876d
Andrew Johnson <ajohnson.uva@gmail.com>
2018-10-19 13:28:54 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,13 +1,14 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
-winner = ""
+
 def select_winner(passengers)
+  winner = ""
   passengers.each do |suite, name|
     if suite = "suite_a" && name.starts_with?("A")
       winner = name
